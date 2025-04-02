7caeea6595fb663e2b21146e3adb83d90cb75e28
Anuj More <moreofme7777@gmail.com>
2025-04-02 12:38:41 +0530

    Fetch details of the movie when clicked 'Details', and render the ui accordingly to accomodate the new data.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,29 @@ import { SetWatchlistContext, WatchlistContext } from '../contexts/MoviesContext
 const MovieCard = ({ movie, isWatchlist }) => {
   const setWatchlist = useContext(SetWatchlistContext)
   const watchlist = useContext(WatchlistContext)
+  let [movieDetails, setMovieDetails] = useState(null)
 
   const [isInDetails, setIsInDetails] = useState(false)
+
+  const fetchMovieDetails = async () => {
+    const API_URL = `https://www.omdbapi.com/?apikey=40524c65&t=${movie.Title}`
+
+    try {
+      const response = await fetch(API_URL)
+      const data = await response.json()
+
+      if (data.Response === "True") {
+        setMovieDetails(data)
+        setIsInDetails(true) // Show details modal
+      }
+    } catch (error) {
+      console.log("Error fetching movie details:", error)
+    }
+  }
+
+  useEffect(() => {
+    console.log(movieDetails)
+  }, [movieDetails])
 
   useEffect(() => {
     localStorage.setItem('watchlist', JSON.stringify(watchlist))
@@ -32,15 +53,16 @@ const MovieCard = ({ movie, isWatchlist }) => {
   } else if (isInDetails) {
     return (
       <li className='border p-4'>
-        <img src={movie.Poster} width={250}/>
-        <h5 className="w-[250px]">{movie.Title} ({movie.Year})</h5>
-        <h6>Type: {movie.Type}</h6>
+        <img src={movieDetails.Poster} width={250}/>
+        <h5 className="w-[250px]">{movieDetails.Title} ({movieDetails.Year})</h5>
+        <h6>Type: {movieDetails.Type} Genre: {movieDetails.Genre}</h6>
+        <div>Rating: {movieDetails.imdbRating}</div>
         <button 
           className='border px-2 py-1'
           onClick={() => {
             let flag = false
             watchlist.forEach(element => {
-              if(element.imdbID === movie.imdbID){
+              if(element.imdbID === movieDetails.imdbID){
                 flag = true
               }
             });
@@ -70,7 +92,7 @@ const MovieCard = ({ movie, isWatchlist }) => {
     return (
       <li className='border p-4'>
         <div>
-          {movie.Title}
+          {movie.Title} ({movie.Year})
         </div>
 
         <div>
@@ -97,9 +119,7 @@ const MovieCard = ({ movie, isWatchlist }) => {
 
           <button 
             className='border px-2 py-1 ml-2'
-            onClick={() => {
-              setIsInDetails(true)
-            }}
+            onClick={fetchMovieDetails}
           > 
             Details
           </button>
